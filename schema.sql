77b2644f176628315c9ee04b5ce579cbce179660
Juanamct <107228424+Juanamct@users.noreply.github.com>
2022-07-20 14:52:40 -0400

    updated code
    adjusted some of the create table formats

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 -- Creating tables for PH-EmployeeDB
+-- Create departments table
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -6,7 +7,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
-
+-- Create employees table
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
@@ -17,6 +18,8 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
+-- Create dept_manager table.  Could not do csv data import until
+-- employees csv data table was imported.
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -27,6 +30,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+-- Create salaries table.  Notice all "keys" are the same
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -36,6 +40,7 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+-- Create dept_emp table, similar to dept_manager table
 CREATE TABLE dept_emp (
 emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
@@ -46,11 +51,24 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+-- Create titles table.  *Used foreign key only, data table would not import with 
+-- primary key due to emp_no not being unique in table.
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+  );
+
+--Confirming tables were created successfully
+SELECT * FROM departments;
+
+
+
+
+
+
+
+
+
